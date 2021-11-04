8686a2cdd41031b69834f43ea8cc1bd4efc93a2e
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-04 13:38:20 +0600

    create component app-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoList';
+  tasks = ['Buy milk', 'Walk with dog', 'do homework'];
 }
