4d7be0d075c1aa70ab08c787774e474879917008
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-04 18:06:11 +0600

    add checkbox(done tasks)

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -6,6 +6,8 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent {
+  isFocusInput = false;
+  isChecked = false;
   @Input() taskMessage = '';
   @Output() deleteTask = new EventEmitter();
   @Output() changeTask = new EventEmitter<string>();
@@ -15,5 +17,21 @@ export class TaskComponent {
   taskMessageInput(event: Event) {
     const target = <HTMLInputElement>event.target;
     this.changeTask.emit(target.value);
+  }
+  onFocusOnInput() {
+    this.isFocusInput = true;
+  }
+  onFocusOutInput() {
+    this.isFocusInput = false;
+  }
+  onCheckboxChange() {
+    this.isChecked = !this.isChecked;
+  }
+  className() {
+    return {
+      'form-control':true,
+      'border-0 border-bottom border-2 border-danger': this.isFocusInput,
+      'text-success': this.isChecked,
+    }
   }
 }
